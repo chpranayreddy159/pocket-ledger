9055af97ff03055f39ded5ea2efed17d7d234241
chpranayreddy159 <41339318+chpranayreddy159@users.noreply.github.com>
2026-07-22 17:43:49 +0000

    Add transaction model

---
+++ lib/shared/models/transaction_model.dart
@@ -0,0 +1,43 @@
+class TransactionModel {
+  final int? id;
+  final String title;
+  final double amount;
+  final String type;
+  final String category;
+  final String date;
+  final String note;
+
+  TransactionModel({
+    this.id,
+    required this.title,
+    required this.amount,
+    required this.type,
+    required this.category,
+    required this.date,
+    required this.note,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'amount': amount,
+      'type': type,
+      'category': category,
+      'date': date,
+      'note': note,
+    };
+  }
+
+  factory TransactionModel.fromMap(Map<String, dynamic> map) {
+    return TransactionModel(
+      id: map['id'],
+      title: map['title'],
+      amount: map['amount'],
+      type: map['type'],
+      category: map['category'],
+      date: map['date'],
+      note: map['note'],
+    );
+  }
+}
