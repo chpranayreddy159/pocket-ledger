4c141aacc59de889b3899a8b99c116711de6c91f
chpranayreddy159 <41339318+chpranayreddy159@users.noreply.github.com>
2026-07-22 17:42:18 +0000

    Add transaction table constants

---
+++ lib/core/database/tables.dart
@@ -0,0 +1,11 @@
+class Tables {
+  static const String transactions = 'transactions';
+
+  static const String id = 'id';
+  static const String title = 'title';
+  static const String amount = 'amount';
+  static const String type = 'type';
+  static const String category = 'category';
+  static const String date = 'date';
+  static const String note = 'note';
+}
